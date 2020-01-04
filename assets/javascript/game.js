5196bd9cef861568813fddf0d4aa0195adbe6e2a
Jack Lawless <Lawless.jackm@gmail.com>
2020-01-04 07:54:29 -0600

    final completed game and files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,11 @@ var wins = 0;
 var losses = 0;
 
 
-var crystalone = Math.floor((Math.random() * 12)+1); //random number between 1-12
-var crystaltwo = Math.floor((Math.random() * 12)+1);
-var crystalthree = Math.floor((Math.random() * 12)+1);
-var crystalfour = Math.floor((Math.random() * 12)+1);
-var numbertoget = Math.floor(Math.random() * (121-19)+19); //random number between 19-120, max value has to be one more than the range you want
+var crystalone = Math.floor((Math.random() * 12) + 1); //random number between 1-12
+var crystaltwo = Math.floor((Math.random() * 12) + 1);
+var crystalthree = Math.floor((Math.random() * 12) + 1);
+var crystalfour = Math.floor((Math.random() * 12) + 1);
+var numbertoget = Math.floor(Math.random() * (121 - 19) + 19); //random number between 19-120, max value has to be one more than the range you want
 var yourscore = 0;
 
 
@@ -36,58 +36,47 @@ imageCrystal4.attr("src", "assets/images/crystal4.jpg");
 imageCrystal4.attr("data-crystalvalue", crystalfour);
 $("#crystals").append(imageCrystal4);
 
-function resetgame(){
-  crystalone = Math.floor((Math.random() * 12)+1); //recalculate all variables and set scores back to 0
-  crystaltwo = Math.floor((Math.random() * 12)+1);
-  crystalthree = Math.floor((Math.random() * 12)+1);
-  crystalfour = Math.floor((Math.random() * 12)+1);
-  numbertoget = Math.floor(Math.random() * (121-19)+19); 
-  yourscore = 0; 
+$("#your-score").text(yourscore);
+$("#number-to-reach").text(numbertoget);
+
+function resetgame() {
+  crystalone = Math.floor((Math.random() * 12) + 1); //recalculate all variables and set scores back to 0
+  crystaltwo = Math.floor((Math.random() * 12) + 1);
+  crystalthree = Math.floor((Math.random() * 12) + 1);
+  crystalfour = Math.floor((Math.random() * 12) + 1);
+  numbertoget = Math.floor(Math.random() * (121 - 19) + 19);
+  yourscore = 0;
   $("#number-to-reach").text(numbertoget);
   $("#your-score").text(yourscore);
-  imageCrystal.attr("data-crystalvalue", crystalone); //need to recalculate the variables AFTER they are calculated the first time
+  imageCrystal.attr("data-crystalvalue", crystalone); //need to reset the variables after they are calculated the first time
   imageCrystal2.attr("data-crystalvalue", crystaltwo);
   imageCrystal3.attr("data-crystalvalue", crystalthree);
   imageCrystal4.attr("data-crystalvalue", crystalfour);
-  }
+}
 
 
-$("#your-score").text(yourscore);
+$(document).ready(function () {
+  $(".crystal-image").on("click", function () {
 
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
 
-$("#number-to-reach").text(numbertoget);
+    yourscore += crystalValue;
 
-console.log(numbertoget,crystalone,crystaltwo,crystalthree,crystalfour)
-
-$(document).ready(function() {
-    $(".crystal-image").on("click", function() {
-
-        // Determining the crystal's value requires us to extract the value from the data attribute.
-        // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-        // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-        // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-    
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
-        // We then add the crystalValue to the user's "counter" which is a global variable.
-        // Every click, from every crystal adds to the global counter.
-        yourscore += crystalValue;
-        console.log("crystal value =" +crystalValue)
-        console.log(numbertoget,crystalone,crystaltwo,crystalthree,crystalfour)
-        $("#your-score").text(yourscore);
-        if (yourscore === numbertoget) {
-          wins++
-          $("#winstxt").text("wins: " + wins);
-          resetgame()
-        }
-    
-        else if (yourscore >= numbertoget) {
-          losses++
-          $("#lossestxt").text("losses: " +losses);
-          resetgame()
-        }
-        
-      }
-      )
+    $("#your-score").text(yourscore);
+    if (yourscore === numbertoget) {
+      wins++
+      $("#winstxt").text("wins: " + wins);
+      resetgame()
     }
-    )   
+
+    else if (yourscore >= numbertoget) {
+      losses++
+      $("#lossestxt").text("losses: " + losses);
+      resetgame()
+    }
+
+  }
+  )
+}
+)   
